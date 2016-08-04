f6a1d62cae0650f3321235550ea28f323cb2490c
Anita Wu <awu@bullhorn.com>
2016-08-04 13:27:16 -0700

    Updating unit tests

--- src/test/java/com/bullhorn/dataloader/task/LoadAttachmentTaskTest.java
+++ src/test/java/com/bullhorn/dataloader/task/LoadAttachmentTaskTest.java
@@ -58,9 +58,6 @@ public class LoadAttachmentTaskTest {
 
     @Before
     public void setUp() throws Exception {
-        String path = getFilePath("PropertyFileUtilTest.properties");
-
-        propertyFileUtil = new LoadAttachmentPropertyFileUtil(path);
         csvFileWriter = Mockito.mock(CsvFileWriter.class);
         bullhornData = Mockito.mock(BullhornData.class);
         actionTotals = Mockito.mock(ActionTotals.class);
@@ -69,32 +66,41 @@ public class LoadAttachmentTaskTest {
         // Capture arguments to the writeRow method - this is our output from the deleteTask run
         resultArgumentCaptor = ArgumentCaptor.forClass(Result.class);
 
-        dataMap = new LinkedHashMap<String, String>();
-        dataMap.put("externalID","1001");
-        dataMap.put("relativeFilePath","testResume/Test Resume.doc");
-        dataMap.put("isResume","0");
-
-        dataMap2 = new LinkedHashMap<String, String>();
-        dataMap2.put("id","2011");
-        dataMap2.put("relativeFilePath","testResume/Test Resume2.doc");
-        dataMap2.put("isResume","1");
-
+        // make sure candidateExistField is set properly for these tests
+        String path = getFilePath("PropertyFileUtilTest.properties");
         FileInputStream fileInputStream = new FileInputStream(path);
         Properties properties = new Properties();
         properties.load(fileInputStream);
         fileInputStream.close();
-        properties.setProperty("candidateAttachmentExistField","id");
+        properties.setProperty("candidateExistField","id");
+        propertyFileUtil = new LoadAttachmentPropertyFileUtil(path, properties);
+
+        fileInputStream = new FileInputStream(path);
+        properties = new Properties();
+        properties.load(fileInputStream);
+        fileInputStream.close();
+        properties.setProperty("candidateExistField","externalID");
         propertyFileUtil2 = new LoadAttachmentPropertyFileUtil(path, properties);
+
+        dataMap = new LinkedHashMap<String, String>();
+        dataMap.put("id","1001");
+        dataMap.put("relativeFilePath","testResume/Test Resume.doc");
+        dataMap.put("isResume","0");
+
+        dataMap2 = new LinkedHashMap<String, String>();
+        dataMap2.put("externalID","2011Ext");
+        dataMap2.put("relativeFilePath","testResume/Test Resume2.doc");
+        dataMap2.put("isResume","1");
     }
 
     @Test
     public void loadAttachmentSuccessTest() throws Exception {
-        final String[] expectedValues = {"1001", "testResume/Test Resume.doc", "0", "1"};
+        final String[] expectedValues = {"1001", "testResume/Test Resume.doc", "0", "1001"};
         final Result expectedResult = Result.Insert(0);
         task = new LoadAttachmentTask(Command.LOAD_ATTACHMENTS, 1, Candidate.class, dataMap, csvFileWriter, propertyFileUtil, bullhornData, printUtil, actionTotals);
 
         final List<Candidate> candidates = new ArrayList<>();
-        candidates.add(new Candidate(1));
+        candidates.add(new Candidate(1001));
 
         final ListWrapper<Candidate> listWrapper = new CandidateListWrapper();
         listWrapper.setData(candidates);
@@ -103,7 +109,8 @@ public class LoadAttachmentTaskTest {
         final FileMeta mockedFileMeta = Mockito.mock(FileMeta.class);
         final StandardFileWrapper fileWrapper = new StandardFileWrapper(mockedFileContent, mockedFileMeta);
 
-        when(bullhornData.search(anyObject(), anyString(), anySet(), anyObject())).thenReturn(listWrapper);
+        //when(bullhornData.search(anyObject(), anyString(), anySet(), anyObject())).thenReturn(listWrapper);
+        when(bullhornData.search(anyObject(), eq("id:1001"), anySet(), anyObject())).thenReturn(listWrapper);
         when(bullhornData.addFile(anyObject(), anyInt(), any(File.class), anyString(), anyObject(), anyBoolean())).thenReturn(fileWrapper);
 
         task.run();
@@ -116,12 +123,12 @@ public class LoadAttachmentTaskTest {
 
     @Test
     public void loadAttachmentFailureTest() throws ExecutionException, IOException {
-        final String[] expectedValues = {"1001", "testResume/Test Resume.doc", "0", "1"};
+        final String[] expectedValues = {"1001", "testResume/Test Resume.doc", "0", "1001"};
         final Result expectedResult = Result.Failure(new RestApiException("Test").toString());
         task = new LoadAttachmentTask(Command.LOAD_ATTACHMENTS, 1, Candidate.class, dataMap, csvFileWriter, propertyFileUtil, bullhornData, printUtil, actionTotals);
 
         final List<Candidate> candidates = new ArrayList<>();
-        candidates.add(new Candidate(1));
+        candidates.add(new Candidate(1001));
 
         final ListWrapper<Candidate> listWrapper = new CandidateListWrapper();
         listWrapper.setData(candidates);
@@ -130,7 +137,7 @@ public class LoadAttachmentTaskTest {
         final FileMeta mockedFileMeta = Mockito.mock(FileMeta.class);
         final StandardFileWrapper fileWrapper = new StandardFileWrapper(mockedFileContent, mockedFileMeta);
 
-        when(bullhornData.search(anyObject(), anyString(), anySet(), anyObject())).thenReturn(listWrapper);
+        when(bullhornData.search(anyObject(), eq("id:1001"), anySet(), anyObject())).thenReturn(listWrapper);
         when(bullhornData.addFile(anyObject(), anyInt(), any(File.class), anyString(), anyObject(), anyBoolean())).thenThrow(new RestApiException("Test"));
 
         task.run();
@@ -142,17 +149,14 @@ public class LoadAttachmentTaskTest {
     }
 
     @Test
-    public void loadAttachmentWithIdSuccessTest() throws Exception {
-        final String[] expectedValues = {"2011", "testResume/Test Resume2.doc", "1", "1"};
+    public void existPropertyConfiguredCorrectlyTest() throws Exception {
+        final String[] expectedValues = {"2011Ext", "testResume/Test Resume2.doc", "1", "1001"};
         final Result expectedResult = Result.Insert(0);
 
-        final String path = getFilePath("PropertyFileUtilTest.properties");
-        final PropertyFileUtil propertyFileUtilTest = new PropertyFileUtil(path);
-
-        task = new LoadAttachmentTask(Command.LOAD_ATTACHMENTS, 1, Candidate.class, dataMap2, csvFileWriter, propertyFileUtilTest, bullhornData, printUtil, actionTotals);
+        task = new LoadAttachmentTask(Command.LOAD_ATTACHMENTS, 1, Candidate.class, dataMap2, csvFileWriter, propertyFileUtil2, bullhornData, printUtil, actionTotals);
 
         final List<Candidate> candidates = new ArrayList<>();
-        candidates.add(new Candidate(1));
+        candidates.add(new Candidate(1001));
 
         final ListWrapper<Candidate> listWrapper = new CandidateListWrapper();
         listWrapper.setData(candidates);
@@ -161,7 +165,7 @@ public class LoadAttachmentTaskTest {
         final FileMeta mockedFileMeta = Mockito.mock(FileMeta.class);
         final StandardFileWrapper fileWrapper = new StandardFileWrapper(mockedFileContent, mockedFileMeta);
 
-        when(bullhornData.search(anyObject(), anyString(), anySet(), anyObject())).thenReturn(listWrapper);
+        when(bullhornData.search(anyObject(), eq("externalID:\"2011Ext\""), anySet(), anyObject())).thenReturn(listWrapper);
         when(bullhornData.addFile(anyObject(), anyInt(), any(File.class), anyString(), anyObject(), anyBoolean())).thenReturn(fileWrapper);
 
         task.run();
