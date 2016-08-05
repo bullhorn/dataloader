6bba3e50aec85752e8bc2db7c8927ac8ddbfe398
Tim Kennedy <t.kennedy@bullhorn.com>
2016-08-04 21:35:04 -0500

    Takes into account immutability for trimming. Takes into account leading and trailing spaces between multiple values in entityExistFields.

--- src/main/java/com/bullhorn/dataloader/util/validation/PropertyValidation.java
+++ src/main/java/com/bullhorn/dataloader/util/validation/PropertyValidation.java
@@ -10,49 +10,56 @@ import java.util.Set;
 public final class PropertyValidation {
 
 	public String validateUsername(String username) {
-		if(username.trim().isEmpty()) {
+		String trimmedUsername = username.trim();
+		if(trimmedUsername.isEmpty()) {
 			throw new IllegalArgumentException("DataLoader Properties Error: username property must not be blank");
 		}
 		return username;
 	}
 
 	public String validatePassword(String password) {
-		if(password.trim().isEmpty()) {
+		String trimmedPassword = password.trim();
+		if(trimmedPassword.isEmpty()) {
 			throw new IllegalArgumentException("DataLoader Properties Error: password property must not be blank");
 		}
 		return password;
 	}
 
 	public String validateClientId(String clientId) {
-		if(clientId.trim().isEmpty()) {
+		String trimmedClientId = clientId.trim();
+		if(trimmedClientId.isEmpty()) {
 			throw new IllegalArgumentException("DataLoader Properties Error: clientId property must not be blank");
 		}
 		return clientId;
 	}
 
 	public String validateClientSecret(String clientSecret) {
-		if(clientSecret.trim().isEmpty()) {
+		String trimmedClientSecret = clientSecret.trim();
+		if(trimmedClientSecret.isEmpty()) {
 			throw new IllegalArgumentException("DataLoader Properties Error: clientSecret property must not be blank");
 		}
 		return clientSecret;
 	}
 
 	public String validateAuthorizeUrl(String authorizeUrl) {
-		if(authorizeUrl.trim().isEmpty()) {
+		String trimmedAuthorizeUrl = authorizeUrl.trim();
+		if(trimmedAuthorizeUrl.isEmpty()) {
 			throw new IllegalArgumentException("DataLoader Properties Error: authorizeUrl property must not be blank");
 		}
 		return authorizeUrl;
 	}
 
 	public String validateTokenUrl(String tokenUrl) {
-		if(tokenUrl.trim().isEmpty()) {
+		String trimmedTokenUrl = tokenUrl.trim();
+		if(trimmedTokenUrl.isEmpty()) {
 			throw new IllegalArgumentException("DataLoader Properties Error: tokenUrl property must not be blank");
 		}
 		return tokenUrl;
 	}
 
 	public String validateLoginUrl(String loginUrl) {
-		if(loginUrl.trim().isEmpty()) {
+		String trimmedLoginUrl = loginUrl.trim();
+		if(trimmedLoginUrl.isEmpty()) {
 			throw new IllegalArgumentException("DataLoader Properties Error: loginUrl property must not be blank");
 		}
 		return loginUrl;
@@ -61,11 +68,12 @@ public final class PropertyValidation {
 	public void validateFrontLoadedEntities(Set<String> frontLoadedEntities) {
 		final Set<String> entitiesFormatted = new HashSet<>();
 		for(Entity entity : Entity.values()) {
-			entitiesFormatted.add(entity.getEntityName().replaceAll("_", "").trim());
+			entitiesFormatted.add(entity.getEntityName().trim().replaceAll("_", ""));
 		}
 
 		for(String frontLoadedEntity : frontLoadedEntities) {
-			if(!entitiesFormatted.contains(frontLoadedEntity)) {
+			String trimmedFrontLoadedEntity = frontLoadedEntity.trim();
+			if(!entitiesFormatted.contains(trimmedFrontLoadedEntity)) {
 				throw new IllegalArgumentException("DataLoader Properties Error: frontLoadedEntities property contains invalid entity name: " + frontLoadedEntity);
 			}
 		}
@@ -73,14 +81,19 @@ public final class PropertyValidation {
 
 	public void validateEntityExistFields(Map<String, List<String>> entityExistFieldsMap) {
 		for(Map.Entry<String, List<String>> entity : entityExistFieldsMap.entrySet() ) {
-			if(entity.getValue().get(0).trim().equals("")) {
+			for(String value : entity.getValue()) {
+				String trimmed = value.trim();
+				entity.getValue().set(entity.getValue().indexOf(value), trimmed);
+			}
+			if(entity.getValue().get(0).equals("")) {
 				throw new IllegalArgumentException("DataLoader Properties Error: " + entity.getKey() + " property must not be blank");
 			}
 		}
 	}
 
 	public String validateListDelimiter(String listDelimiter) {
-		if(listDelimiter.trim().isEmpty()) {
+		String trimmedListDelimiter = listDelimiter.trim();
+		if(trimmedListDelimiter.isEmpty()) {
 			throw new IllegalArgumentException("DataLoader Properties Error: listDelimiter property must not be blank");
 		}
 		return listDelimiter;
