7d305bd42584e557afe452d179bc083996c01f38
Nathan Dickerson <ndickerson@bullhorn.com>
2016-08-23 11:21:31 -0500

    Added ability to override any of the properties from the propertyfile with system -Dargs
    This is to accomodate calling dataloader programatically from Jenkins, and it enables extra flexibility in the future for
    overriding fields from a property file at runtime if desired.

--- src/test/java/com/bullhorn/dataloader/util/PropertyFileUtilTest.java
+++ src/test/java/com/bullhorn/dataloader/util/PropertyFileUtilTest.java
@@ -8,53 +8,104 @@ import org.junit.Test;
 import org.mockito.Mockito;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Properties;
 
 public class PropertyFileUtilTest {
 
     private PropertyValidation propertyValidation;
     private PrintUtil printUtilMock;
     private PropertyFileUtil propertyFileUtil;
+    private Properties systemPropertiesMock;
+    private String path;
 
     @Before
-    public void setup() throws IOException {
+    public void setup() {
         propertyValidation = new PropertyValidation();
         printUtilMock = Mockito.mock(PrintUtil.class);
-
-        String path = getFilePath("dataloader.properties");
-        propertyFileUtil = new PropertyFileUtil(path, propertyValidation, printUtilMock);
+        systemPropertiesMock = Mockito.mock(Properties.class);
+        path = getFilePath("dataloader.properties");
     }
 
     @Test
-    public void testGetters() {
-        Assert.assertEquals(propertyFileUtil.getUsername(), "john.smith");
-        Assert.assertEquals(propertyFileUtil.getPassword(), "password123");
-        Assert.assertEquals(propertyFileUtil.getClientId(), "1234abcd-123a-123a-123a-acbd1234567");
-        Assert.assertEquals(propertyFileUtil.getClientSecret(), "1234567890abcdefghijklmn");
-        Assert.assertEquals(propertyFileUtil.getAuthorizeUrl(), "https://auth.bullhornstaffing.com/oauth/authorize");
-        Assert.assertEquals(propertyFileUtil.getTokenUrl(), "https://auth.bullhornstaffing.com/oauth/token");
-        Assert.assertEquals(propertyFileUtil.getLoginUrl(), "https://rest.bullhornstaffing.com/rest-services/login");
-        Assert.assertEquals(propertyFileUtil.getListDelimiter(), ";");
-        Assert.assertEquals(propertyFileUtil.getDateParser(), DateTimeFormat.forPattern("MM/dd/yyyy HH:mm:ss.SSS"));
-        Assert.assertEquals(propertyFileUtil.getNumThreads(), new Integer(10));
+    public void testPropertyFileGetters() throws IOException {
+        propertyFileUtil = new PropertyFileUtil(path, systemPropertiesMock, propertyValidation, printUtilMock);
+
+        Assert.assertEquals("john.smith", propertyFileUtil.getUsername());
+        Assert.assertEquals("password123", propertyFileUtil.getPassword());
+        Assert.assertEquals("1234abcd-123a-123a-123a-acbd1234567", propertyFileUtil.getClientId());
+        Assert.assertEquals("1234567890abcdefghijklmn", propertyFileUtil.getClientSecret());
+        Assert.assertEquals("https://auth.bullhornstaffing.com/oauth/authorize", propertyFileUtil.getAuthorizeUrl());
+        Assert.assertEquals("https://auth.bullhornstaffing.com/oauth/token", propertyFileUtil.getTokenUrl());
+        Assert.assertEquals("https://rest.bullhornstaffing.com/rest-services/login", propertyFileUtil.getLoginUrl());
+        Assert.assertEquals(";", propertyFileUtil.getListDelimiter());
+        Assert.assertEquals(DateTimeFormat.forPattern("MM/dd/yyyy HH:mm:ss.SSS"), propertyFileUtil.getDateParser());
+        Assert.assertEquals(new Integer(10), propertyFileUtil.getNumThreads());
+    }
+
+    @Test
+    public void testGetterSystemPropertyOverrides() throws IOException {
+        Mockito.doReturn("johnny.appleseed").when(systemPropertiesMock).getProperty("username");
+        Mockito.doReturn("password456").when(systemPropertiesMock).getProperty("password");
+        Mockito.doReturn("2234abcd-123a-123a-123a-acbd1234567").when(systemPropertiesMock).getProperty("clientId");
+        Mockito.doReturn("2234567890abcdefghijklmn").when(systemPropertiesMock).getProperty("clientSecret");
+        Mockito.doReturn("https://auth.bullhornstaffing.com/oauth/apple").when(systemPropertiesMock).getProperty("authorizeUrl");
+        Mockito.doReturn("https://auth.bullhornstaffing.com/oauth/banana").when(systemPropertiesMock).getProperty("tokenUrl");
+        Mockito.doReturn("https://rest.bullhornstaffing.com/rest-services/cherry").when(systemPropertiesMock).getProperty("loginUrl");
+        Mockito.doReturn(",").when(systemPropertiesMock).getProperty("listDelimiter");
+        Mockito.doReturn("5").when(systemPropertiesMock).getProperty("numThreads");
+
+        propertyFileUtil = new PropertyFileUtil(path, systemPropertiesMock, propertyValidation, printUtilMock);
+
+        Assert.assertEquals("johnny.appleseed", propertyFileUtil.getUsername());
+        Assert.assertEquals("password456", propertyFileUtil.getPassword());
+        Assert.assertEquals("2234abcd-123a-123a-123a-acbd1234567", propertyFileUtil.getClientId());
+        Assert.assertEquals("2234567890abcdefghijklmn", propertyFileUtil.getClientSecret());
+        Assert.assertEquals("https://auth.bullhornstaffing.com/oauth/apple", propertyFileUtil.getAuthorizeUrl());
+        Assert.assertEquals("https://auth.bullhornstaffing.com/oauth/banana", propertyFileUtil.getTokenUrl());
+        Assert.assertEquals("https://rest.bullhornstaffing.com/rest-services/cherry", propertyFileUtil.getLoginUrl());
+        Assert.assertEquals(",", propertyFileUtil.getListDelimiter());
+        Assert.assertEquals(DateTimeFormat.forPattern("MM/dd/yyyy HH:mm:ss.SSS"), propertyFileUtil.getDateParser());
+        Assert.assertEquals(new Integer(5), propertyFileUtil.getNumThreads());
     }
 
     @Test
     public void testExistsFields() throws IOException {
-        Assert.assertEquals(propertyFileUtil.getEntityExistFields("BusinessSector"),
-            Optional.ofNullable(Arrays.asList(new String[]{"name"})));
-        Assert.assertEquals(propertyFileUtil.getEntityExistFields("Candidate"),
-            Optional.ofNullable(Arrays.asList(new String[]{"id"})));
-        Assert.assertEquals(propertyFileUtil.getEntityExistFields("Category"),
-            Optional.ofNullable(Arrays.asList(new String[]{"occupation"})));
-        Assert.assertEquals(propertyFileUtil.getEntityExistFields("JobOrder"),
-            Optional.ofNullable(Arrays.asList(new String[]{"title", "name"})));
+        propertyFileUtil = new PropertyFileUtil(path, systemPropertiesMock, propertyValidation, printUtilMock);
+
+        Assert.assertEquals(Optional.ofNullable(Arrays.asList(new String[]{"name"})),
+            propertyFileUtil.getEntityExistFields("BusinessSector"));
+        Assert.assertEquals(Optional.ofNullable(Arrays.asList(new String[]{"id"})),
+            propertyFileUtil.getEntityExistFields("Candidate"));
+        Assert.assertEquals(Optional.ofNullable(Arrays.asList(new String[]{"occupation"})),
+            propertyFileUtil.getEntityExistFields("Category"));
+        Assert.assertEquals(Optional.ofNullable(Arrays.asList(new String[]{"title", "name"})),
+            propertyFileUtil.getEntityExistFields("JobOrder"));
 
         Assert.assertFalse(propertyFileUtil.getEntityExistFields("businessSector").isPresent());
         Assert.assertFalse(propertyFileUtil.getEntityExistFields("CandidateName").isPresent());
         Assert.assertFalse(propertyFileUtil.getEntityExistFields("BOGUS").isPresent());
+    }
+
+    @Test
+    public void testExistsFieldSystemPropertyOverrides() throws IOException {
+        Mockito.doReturn("customTextField4,customTextField5").when(systemPropertiesMock).getProperty("candidateExistField");
+        Mockito.doReturn(new HashSet<>(Arrays.asList("candidateExistField", "clientContactExistField"))).when(systemPropertiesMock).stringPropertyNames();
+
+        propertyFileUtil = new PropertyFileUtil(path, systemPropertiesMock, propertyValidation, printUtilMock);
+
+        Assert.assertEquals(Optional.ofNullable(Arrays.asList(new String[]{"customTextField4", "customTextField5"})),
+            propertyFileUtil.getEntityExistFields("Candidate"));
+    }
+
+    @Test(expected = FileNotFoundException.class)
+    public void testPropertyFileSystemPropertyOverride() throws IOException {
+        Mockito.doReturn("bogus/file/path/to/dataloader.properties").when(systemPropertiesMock).getProperty("propertyfile");
+        propertyFileUtil = new PropertyFileUtil(path, systemPropertiesMock, propertyValidation, printUtilMock);
     }
 
     private String getFilePath(String filename) {
