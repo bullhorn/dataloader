196069252b7c535a6f864606925fcee88a8e5a2d
Tim Kennedy <t.kennedy@bullhorn.com>
2016-08-25 13:38:17 -0500

    Unit tests for associated field logic

--- src/main/java/com/bullhorn/dataloader/util/TemplateUtil.java
+++ src/main/java/com/bullhorn/dataloader/util/TemplateUtil.java
@@ -13,7 +13,6 @@ import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
