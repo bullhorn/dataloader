389acfcd6f8e75dc0db84b57afccf4b302e396e2
Adam Crowe <acrowe@bullhorn.com>
2016-08-30 15:33:55 -0500

    add countryName to list of address fields

--- src/main/java/com/bullhorn/dataloader/task/AbstractTask.java
+++ src/main/java/com/bullhorn/dataloader/task/AbstractTask.java
@@ -263,7 +263,7 @@ public abstract class AbstractTask<B extends BullhornEntity> implements Runnable
     }
 
     private boolean isAddressField(String field) {
-        List<String> addressFields = Arrays.asList("address1", "address2", "city", "state", "zip", "countryid");
+        List<String> addressFields = Arrays.asList("address1", "address2", "city", "state", "zip", "countryid", "countryName");
         return addressFields.indexOf(field.toLowerCase()) > -1;
     }
 
