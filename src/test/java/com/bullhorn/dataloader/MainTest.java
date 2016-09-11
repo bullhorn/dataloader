d29e9ab2473f2d68d2a5ab832cb498d827805033
Nathan Dickerson <ndickerson@bullhorn.com>
2016-09-11 07:24:53 -0500

    Added test for the main entry point

---
+++ src/test/java/com/bullhorn/dataloader/MainTest.java
@@ -0,0 +1,41 @@
+package com.bullhorn.dataloader;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class MainTest {
+
+    private PrintStream originalSystemOut;
+    private ByteArrayOutputStream outputStream;
+
+    @Before
+    public void setup() {
+        originalSystemOut = System.out;
+
+        // Replace System.out with our own stream for testing
+        outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        System.setOut(printStream);
+    }
+
+    @After
+    public void teardown() {
+        // Set System.out back when we're done
+        System.setOut(originalSystemOut);
+    }
+
+    @Test
+    public void testMain() {
+        String[] args = {"load", "bogusFile.bad"};
+
+        Main main = new Main();
+        main.main(args);
+
+        Assert.assertTrue(outputStream.toString().contains("ERROR: Cannot access: bogusFile.bad"));
+    }
+}
