d7f8863a62dbd18391c674dbcf59a41b86a8598c
Nathan Dickerson <ndickerson@bullhorn.com>
2016-09-11 06:52:43 -0500

    Removed potentially problematic test case

--- src/test/java/com/bullhorn/dataloader/util/TimerTest.java
+++ src/test/java/com/bullhorn/dataloader/util/TimerTest.java
@@ -6,18 +6,8 @@ import org.junit.Test;
 public class TimerTest {
 
     @Test
-    public void testGetDurationMillis() throws InterruptedException {
-        final Timer timer = new Timer();
-        Thread.sleep(1); // Create a 1 msec wait
-        final long durationMillis = timer.getDurationMillis();
-
-        Assert.assertTrue(durationMillis > 0);
-    }
-
-    @Test
     public void testGetDurationStringHMS() throws InterruptedException {
         final Timer timer = new Timer();
-        Thread.sleep(1); // Create a 1 msec wait
         final String durationString = timer.getDurationStringHMS();
 
         Assert.assertEquals("00:00:00", durationString);
@@ -26,7 +16,6 @@ public class TimerTest {
     @Test
     public void testGetDurationStringSec() throws InterruptedException {
         final Timer timer = new Timer();
-        Thread.sleep(1); // Create a 1 msec wait
         final String durationString = timer.getDurationStringSec();
 
         Assert.assertEquals("0.0 sec", durationString);
