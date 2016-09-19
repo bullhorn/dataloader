99709da7204acd9b6303cf296b277390be553c05
Nathan Dickerson <ndickerson@bullhorn.com>
2016-09-19 13:18:18 -0500

    Implemented the creation of an example directory for uploading unique data

--- src/test/java/com/bullhorn/dataloader/integration/IntegrationTest.java
+++ src/test/java/com/bullhorn/dataloader/integration/IntegrationTest.java
@@ -2,12 +2,14 @@ package com.bullhorn.dataloader.integration;
 
 import com.bullhorn.dataloader.Main;
 import com.bullhorn.dataloader.TestUtils;
-import com.bullhorn.dataloader.util.StringConsts;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -43,27 +45,49 @@ public class IntegrationTest {
 
     @Test
     public void loadFromFolder() throws IOException {
-        String examplesDir = "examples/load_" + StringConsts.TIMESTAMP;
-        String externalID = "-ext-" + StringConsts.TIMESTAMP;
-        createExampleDirectory("examples/load", examplesDir, "-ext-1", externalID);
-        String[] args = {"load", examplesDir};
+        long secondsSinceEpoch = System.currentTimeMillis() / 1000;
+        String resourceDirPath = TestUtils.getFilePath("");
+        String examplesDirPath = resourceDirPath + "/integrationTestExamples_" + secondsSinceEpoch;
+        String newExternalIdEnding = "-" + secondsSinceEpoch;
+        File examplesDirectory = createExampleDirectory("examples/load", examplesDirPath, "-ext-1", newExternalIdEnding);
+        String[] args = {"load", examplesDirPath};
 
         Main.main(args);
 
         String cmdLineOutput = consoleOutputCapturer.stop();
-        Assert.assertTrue(cmdLineOutput.contains("ERROR"));
+        Assert.assertFalse(cmdLineOutput.contains("ERROR"));
+        FileUtils.deleteDirectory(examplesDirectory);
     }
 
     /**
      * Given a directory with CSV files in it, this method will move all of those files to a new directory
      * and will string replace all instances in all files of the given text.
      *
-     * @param originalDir The source directory path
-     * @param newDir The path to the new directory to create
+     * @param originalDirPath The source directory path
+     * @param newDirPath The path to the new directory to create
      * @param findText The text to find in the files
      * @param replaceText The text to replace all instances of findText with
+     * @return The new directory that was created
      */
-    private void createExampleDirectory(String originalDir, String newDir, String findText, String replaceText) {
+    private File createExampleDirectory(String originalDirPath, String newDirPath, String findText, String replaceText) throws IOException {
+        File originalDirectory = new File(originalDirPath);
+        File newDirectory = new File(newDirPath);
+        FileUtils.copyDirectory(originalDirectory, newDirectory);
 
+        File[] directoryListing = newDirectory.listFiles();
+        if (directoryListing != null) {
+            for (File file : directoryListing) {
+                String fileExtension = FilenameUtils.getExtension(file.getPath());
+                if (fileExtension.equalsIgnoreCase("csv")) {
+                    String content = FileUtils.readFileToString(file, "UTF-8");
+                    content = content.replaceAll(findText, replaceText);
+                    FileUtils.writeStringToFile(file, content, "UTF-8");
+                }
+            }
+        } else {
+            throw new IllegalArgumentException("Integration Test Failure: Cannot clone the directory: '" + originalDirPath + "' for testing.");
+        }
+
+        return newDirectory;
     }
 }
