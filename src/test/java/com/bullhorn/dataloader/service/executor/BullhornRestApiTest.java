1e877123062563f6f272517860c0deaba3a0f420
Nathan Dickerson <nathan@edickerson.net>
2017-06-23 10:26:41 -0500

    Added testing of BullhornRestApi

--- src/test/java/com/bullhorn/dataloader/service/executor/BullhornRestApiTest.java
+++ src/test/java/com/bullhorn/dataloader/service/executor/BullhornRestApiTest.java
@@ -2,9 +2,11 @@ package com.bullhorn.dataloader.service.executor;
 
 import com.bullhornsdk.data.api.BullhornRestCredentials;
 import com.bullhornsdk.data.exception.RestApiException;
+import com.bullhornsdk.data.model.entity.core.standard.Candidate;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 public class BullhornRestApiTest {
     private BullhornRestCredentials bullhornRestCredentials;
@@ -27,5 +29,17 @@ public class BullhornRestApiTest {
 
         Assert.assertNotNull(actualException);
         Assert.assertEquals(expectedException.getMessage(), actualException.getMessage());
+    }
+
+    @Test
+    public void testDeleteEntity() {
+        BullhornRestApi bullhornRestApiPartialMock = Mockito.mock(BullhornRestApi.class);
+        Mockito.when(bullhornRestApiPartialMock.deleteEntity(Mockito.any(), Mockito.any())).thenCallRealMethod();
+
+        bullhornRestApiPartialMock.deleteEntity(Candidate.class, 1);
+
+        Mockito.verify(bullhornRestApiPartialMock, Mockito.times(1)).deleteEntity(Mockito.eq(Candidate.class), Mockito.eq(1));
+        // TODO: Allow for testing that postDelete was called on the mock RestApiExtension
+        // Mockito.verify(bullhornRestApiExtensionMock, Mockito.times(1)).postDelete(bullhornRestApiPartialMock, Mockito.any());
     }
 }
