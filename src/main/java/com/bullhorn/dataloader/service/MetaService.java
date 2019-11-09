99fbda8faaf29c7394b054b91e356654f1d706a1
Nathan Dickerson <ndickerson@bullhorn.com>
2019-11-08 21:10:30 -0600

    feat(Meta): Improved meta output

--- src/main/java/com/bullhorn/dataloader/service/MetaService.java
+++ src/main/java/com/bullhorn/dataloader/service/MetaService.java
@@ -9,11 +9,9 @@ import com.bullhorn.dataloader.util.ValidationUtil;
 import com.bullhornsdk.data.model.entity.meta.Field;
 import com.bullhornsdk.data.model.entity.meta.MetaData;
 import com.bullhornsdk.data.model.enums.MetaParameter;
-import org.apache.commons.io.FileUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.io.File;
 import java.util.List;
 import java.util.Objects;
 
@@ -37,24 +35,11 @@ public class MetaService implements Action {
         RestApi restApi = restSession.getRestApi();
 
         try {
-            printUtil.printAndLog("Getting Meta for " + Objects.requireNonNull(entityInfo).getEntityName() + "...");
-            MetaData metaData = restApi.getMetaData(entityInfo.getEntityClass(), MetaParameter.FULL, null);
-            List<Field> metaFields = metaData.getFields();
-            JSONArray jsonArray = new JSONArray();
-            metaFields.forEach(field -> {
-                if (field.getDescription() != null) {
-                    JSONObject jsonObject = new JSONObject();
-                    jsonObject.put("name", field.getName());
-                    jsonObject.put("label", field.getLabel());
-                    jsonObject.put("description", field.getDescription());
-                    jsonObject.put("hint", field.getHint());
-                    jsonArray.put(jsonObject);
-                }
-            });
-            String metaString = jsonArray.toString(2);
-            File file = new File(entityInfo.getEntityName() + "Meta.json");
-            FileUtils.writeStringToFile(file, metaString, "UTF-8");
-            printUtil.printAndLog("Generated file: " + file.getName());
+            printUtil.log("Getting meta for " + Objects.requireNonNull(entityInfo).getEntityName() + "...");
+            MetaData meta = restApi.getMetaData(entityInfo.getEntityClass(), MetaParameter.FULL, null);
+            JSONObject jsonMeta = metaToJson(meta);
+            printUtil.print(jsonMeta.toString());
+            printUtil.log("Done generating meta for " + Objects.requireNonNull(entityInfo).getEntityName());
         } catch (Exception e) {
             printUtil.printAndLog("ERROR: Failed to get Meta for " + Objects.requireNonNull(entityInfo).getEntityName());
             printUtil.printAndLog(e);
@@ -65,5 +50,39 @@ public class MetaService implements Action {
     public boolean isValidArguments(String[] args) {
         return ValidationUtil.validateNumArgs(args, 2, printUtil)
             && ValidationUtil.validateEntityName(args[1], printUtil);
+    }
+
+    @SuppressWarnings("unchecked")
+    private JSONObject metaToJson(MetaData metaData) {
+        JSONArray jsonFields = new JSONArray();
+        List<Field> fields = metaData.getFields();
+        for (Field field : fields) {
+            if (field.getDescription() != null) {
+                JSONObject jsonField = new JSONObject();
+                jsonField.put("name", field.getName());
+                jsonField.put("type", field.getType());
+                jsonField.put("dataType", field.getDataType());
+                jsonField.put("dataSpecialization", field.getDataSpecialization());
+                jsonField.put("confidential", field.getConfidential());
+                jsonField.put("optional", field.getOptional());
+                jsonField.put("label", field.getLabel());
+                jsonField.put("required", field.getRequired());
+                jsonField.put("readOnly", field.getReadOnly());
+                jsonField.put("multiValue", field.getMultiValue());
+                jsonField.put("sortOrder", field.getSortOrder());
+                jsonField.put("hint", field.getHint());
+                jsonField.put("description", field.getDescription());
+                if (field.getAssociatedEntity() != null) {
+                    jsonField.put("associatedEntity", metaToJson(field.getAssociatedEntity()));
+                }
+                jsonFields.put(jsonField);
+            }
+        }
+
+        JSONObject jsonMeta = new JSONObject();
+        jsonMeta.put("entity", metaData.getEntity());
+        jsonMeta.put("label", metaData.getLabel());
+        jsonMeta.put("fields", jsonFields);
+        return jsonMeta;
     }
 }
--- src/test/java/com/bullhorn/dataloader/service/MetaServiceTest.java
+++ src/test/java/com/bullhorn/dataloader/service/MetaServiceTest.java
@@ -8,17 +8,17 @@ import com.bullhorn.dataloader.rest.RestSession;
 import com.bullhorn.dataloader.util.PrintUtil;
 import com.bullhornsdk.data.exception.RestApiException;
 import com.bullhornsdk.data.model.entity.core.standard.Candidate;
+import com.bullhornsdk.data.model.entity.core.standard.CorporateUser;
 import com.bullhornsdk.data.model.entity.meta.Field;
 import com.bullhornsdk.data.model.entity.meta.StandardMetaData;
 import com.bullhornsdk.data.model.enums.MetaParameter;
-import org.apache.commons.io.FileUtils;
 import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.Arrays;
 
 import static org.mockito.Matchers.anyString;
@@ -51,41 +51,47 @@ public class MetaServiceTest {
             "Place for general comments about the record", "SCALAR", "String");
         Field customTextField = TestUtils.createField("customText1", "Favorite Food", "What is the person's favorite food?",
             "Useful sometimes", "SCALAR", "String");
+        Field ownerField = TestUtils.createField("owner", "Recruiter", "", "", "TO_ONE", "");
+        StandardMetaData<CorporateUser> corporateUserMeta = new StandardMetaData<>();
+        corporateUserMeta.setEntity("CorporateUser");
+        corporateUserMeta.setLabel("Recruiter");
+        ownerField.setAssociatedEntity(corporateUserMeta);
 
         // Mock out Candidate meta data
         StandardMetaData<Candidate> candidateMeta = new StandardMetaData<>();
         candidateMeta.setEntity("Candidate");
-        candidateMeta.setFields(Arrays.asList(idField, emailField, externalIdField, commentsField, customTextField));
+        candidateMeta.setLabel("Employee");
+        candidateMeta.setFields(Arrays.asList(idField, emailField, externalIdField, commentsField, customTextField, ownerField));
 
         when(restSessionMock.getRestApi()).thenReturn(restApiMock);
         when(restApiMock.getMetaData(Candidate.class, MetaParameter.FULL, null)).thenReturn(candidateMeta);
     }
 
     @Test
-    public void testRunCandidate() throws IOException {
+    public void testRunCandidate() {
         String[] testArgs = {Command.META.getMethodName(), EntityInfo.CANDIDATE.getEntityName()};
+        ArgumentCaptor stringCaptor = ArgumentCaptor.forClass(String.class);
 
         metaService.run(testArgs);
 
-        verify(printUtilMock, times(2)).printAndLog(anyString());
+        verify(printUtilMock, times(1)).log("Getting meta for Candidate...");
+        verify(printUtilMock, times(1)).log("Done generating meta for Candidate");
+        verify(printUtilMock, times(1)).print((String) stringCaptor.capture());
 
-        String fileName = EntityInfo.CANDIDATE.getEntityName() + "Meta.json";
-        File outputFile = new File(fileName);
-        Assert.assertTrue(outputFile.isFile());
-
-        String fileContents = FileUtils.readFileToString(outputFile);
-        JSONArray jsonArray = new JSONArray(fileContents);
-        TestUtils.checkJsonObject(jsonArray.getJSONObject(0), "name,label,description,hint", "email,Email,,");
-        TestUtils.checkJsonObject(jsonArray.getJSONObject(1), "name,label,description,hint", "externalID,External ID,,");
-        TestUtils.checkJsonObject(jsonArray.getJSONObject(2), "name,label,description,hint",
+        String jsonPrinted = (String) stringCaptor.getValue();
+        JSONObject meta = new JSONObject(jsonPrinted);
+        Assert.assertEquals(meta.get("entity"), "Candidate");
+        Assert.assertEquals(meta.get("label"), "Employee");
+        JSONArray fields = meta.getJSONArray("fields");
+        TestUtils.checkJsonObject(fields.getJSONObject(0), "name,label,description,hint", "email,Email,,");
+        TestUtils.checkJsonObject(fields.getJSONObject(1), "name,label,description,hint", "externalID,External ID,,");
+        TestUtils.checkJsonObject(fields.getJSONObject(2), "name,label,description,hint",
             "comments,Comments,General Comments,Place for general comments about the record");
-        TestUtils.checkJsonObject(jsonArray.getJSONObject(3), "name,label,description,hint",
+        TestUtils.checkJsonObject(fields.getJSONObject(3), "name,label,description,hint",
             "customText1,Favorite Food,What is the person's favorite food?,Useful sometimes");
-
-        Assert.assertEquals("email", jsonArray.getJSONObject(0).getString("name"));
-        Assert.assertEquals("Email", jsonArray.getJSONObject(0).getString("label"));
-        Assert.assertEquals("", jsonArray.getJSONObject(0).getString("description"));
-        Assert.assertEquals("", jsonArray.getJSONObject(0).getString("hint"));
+        TestUtils.checkJsonObject(fields.getJSONObject(4), "name,label",
+            "owner,Recruiter");
+        Assert.assertEquals(fields.getJSONObject(4).getJSONObject("associatedEntity").getString("entity"), "CorporateUser");
     }
 
     @Test(expected = RestApiException.class)
@@ -104,7 +110,7 @@ public class MetaServiceTest {
 
         metaService.run(new String[]{Command.META.getMethodName(), "Candidate"});
 
-        verify(printUtilMock, times(1)).printAndLog("Getting Meta for Candidate...");
+        verify(printUtilMock, times(1)).log("Getting meta for Candidate...");
         verify(printUtilMock, times(1)).printAndLog("ERROR: Failed to get Meta for Candidate");
     }
 
